61b5dbaf426d4d82f9399c3a0ce224809bf997b2
Miles Granger <miles59923@gmail.com>
2019-07-12 12:18:06 +0200

    Trial from json Value

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,70 @@
-use serde::Deserialize;
 use std::collections::HashMap;
 
+use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub enum PrimitiveType {
+pub(crate) enum PrimitiveType {
     Integer,
     String,
 }
 
 #[derive(Deserialize)]
-pub struct PropertySpecs {
+pub(crate) struct PropertySpecs {
+
+    #[serde(alias = "Required")]
     pub required: bool,
+
+    #[serde(alias = "Documentation")]
     pub documentation: String,
+
+    #[serde(alias = "PrimitiveType")]
     pub primitive_type: String,
-    pub update_type: String
+
+    #[serde(alias = "UpdateType")]
+    pub update_type: String,
 }
 
-#[derive(Deserialize)]
-pub struct PropertyTypes {
-    pub property_types: HashMap<String, PropertyType>
-}
+type PropertyTypes = HashMap<String, PropertyType>;
 
 #[derive(Deserialize)]
-pub struct PropertyType {
-    pub name: String,
+pub(crate) struct PropertyType {
+    #[serde(alias = "Documentation")]
     pub documentation: String,
-    pub properties: HashMap<String, PropertySpecs>
+
+    #[serde(alias = "Properties")]
+    pub properties: HashMap<String, PropertySpecs>,
 }
-
-
-#[macro_export]
-macro_rules! property_type_from_json {
-}
-
-#[macro_export]
-macro_rules! property_type_to_cf_type {
-}
-
 
 #[cfg(test)]
 mod tests {
 
+    use serde_json::json;
+    use crate::{PropertyType, PropertyTypes};
+
     #[test]
-    fn test_property_type() {
-        property_type!()
+    fn test_PropertyType() {
+        let property_json = json!({
+            "AWS::AppMesh::VirtualRouter.PortMapping": {
+                "Documentation": "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-appmesh-virtualrouter-portmapping.html",
+                "Properties": {
+                    "Port": {
+                        "Required": true,
+                        "Documentation": "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-appmesh-virtualrouter-portmapping.html#cfn-appmesh-virtualrouter-portmapping-port",
+                        "PrimitiveType": "Integer",
+                        "UpdateType": "Mutable"
+                    },
+                    "Protocol": {
+                        "Required": true,
+                        "Documentation": "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-appmesh-virtualrouter-portmapping.html#cfn-appmesh-virtualrouter-portmapping-protocol",
+                        "PrimitiveType": "String",
+                        "UpdateType": "Mutable"
+                    }
+                }
+            }
+        });
+
+        assert!(serde_json::from_value::<PropertyTypes>(property_json.clone()).is_ok());
+        assert!(serde_json::from_value::<PropertyType>(property_json["AWS::AppMesh::VirtualRouter.PortMapping"].clone()).is_ok());
     }
 
 }
-
-/*
-AWS::AppMesh::VirtualRouter.PortMapping: {
-    Documentation: "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-appmesh-virtualrouter-portmapping.html",
-    Properties: {
-        Port: {
-            Required: true,
-            Documentation: "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-appmesh-virtualrouter-portmapping.html#cfn-appmesh-virtualrouter-portmapping-port",
-            PrimitiveType: "Integer",
-            UpdateType: "Mutable"
-        },
-        Protocol: {
-            Required: true,
-            Documentation: "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-appmesh-virtualrouter-portmapping.html#cfn-appmesh-virtualrouter-portmapping-protocol",
-            PrimitiveType: "String",
-            UpdateType: "Mutable"
-        }
-    }
-}
-*/
