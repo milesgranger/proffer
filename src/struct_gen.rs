c88922050e5866bc517d73727a3ab77b993ea004
Miles Granger <miles59923@gmail.com>
2019-09-23 19:57:40 +0200

    Add normalize_whitespace test helper

--- src/struct_gen.rs
+++ src/struct_gen.rs
@@ -51,8 +51,12 @@ mod tests {
 
     use crate::*;
 
+    fn normalize_whitespace(s: &str) -> String {
+        s.split("\n").map(|l| l.trim()).filter(|l| l.len() > 0).collect::<String>()
+    }
+
     #[test]
-    fn test_struct_gen() {
+    fn test_basic_gen() {
         let mut struct_ = Struct::new("Basic", true);
 
         let mut f = Field::new("field1", "String", true);
@@ -62,11 +66,16 @@ mod tests {
 
         struct_.field(Field::new("field2", "usize", true));
         let expected = r#"
-            pub struct Basic {
-                pub field: usize
-            }
+         pub struct Basic {
+            /// Some example documentation
+            #[serde = w]
+            pub field1: String,
+            pub field2: usize,
+         }
         "#.to_owned();
-        println!("{}", struct_.generate());
-        //assert_eq!(src_code, expected);
+        let src_code = struct_.generate();
+        println!("{}", &src_code);
+        assert_eq!(normalize_whitespace(&src_code), normalize_whitespace(&expected));
     }
 }
+
