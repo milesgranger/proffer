91265dca1bb2340786db78839b6e76715edfe055
Miles Granger <miles59923@gmail.com>
2019-09-23 20:33:15 +0200

    Move tests into module

--- src/struct_gen.rs
+++ src/struct_gen.rs
@@ -44,38 +44,3 @@ impl SrcCode for Struct {
         Tera::one_off(template, &context, false).unwrap()
     }
 }
-
-
-#[cfg(test)]
-mod tests {
-
-    use crate::*;
-
-    fn normalize_whitespace(s: &str) -> String {
-        s.split("\n").map(|l| l.trim()).filter(|l| l.len() > 0).collect::<String>()
-    }
-
-    #[test]
-    fn test_basic_gen() {
-        let mut struct_ = Struct::new("Basic", true);
-
-        let mut f = Field::new("field1", "String", true);
-        f.annotations.push("#[serde = w]".to_string());
-        f.docs.push("/// Some example documentation".to_string());
-        struct_.field(f);
-
-        struct_.field(Field::new("field2", "usize", true));
-        let expected = r#"
-         pub struct Basic {
-            /// Some example documentation
-            #[serde = w]
-            pub field1: String,
-            pub field2: usize,
-         }
-        "#.to_owned();
-        let src_code = struct_.generate();
-        println!("{}", &src_code);
-        assert_eq!(normalize_whitespace(&src_code), normalize_whitespace(&expected));
-    }
-}
-
