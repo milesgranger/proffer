9e7976861c0caf7d4b7d8f6044d9976496731afc
Miles Granger <miles59923@gmail.com>
2019-11-02 11:57:36 +0100

    Move _gen files to common module (#36)
    * Move _gen files to common module
    
    * Remove _gen suffix from gen modules

---
+++ src/gen/mod.rs
@@ -0,0 +1,21 @@
+//! Re-exports from the `gen` submodules.
+
+pub mod associated_types;
+pub mod r#enum;
+pub mod field;
+pub mod function;
+pub mod generics;
+pub mod r#impl;
+pub mod module;
+pub mod r#struct;
+pub mod r#trait;
+
+pub use associated_types::*;
+pub use field::*;
+pub use function::*;
+pub use generics::*;
+pub use module::*;
+pub use r#enum::*;
+pub use r#impl::*;
+pub use r#struct::*;
+pub use r#trait::*;
