0d9590e81ee362dda71ab3bb3ba342596c54960b
Miles Granger <miles59923@gmail.com>
2019-11-02 09:34:55 +0100

    Fix formatting

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -11,11 +11,11 @@ pub mod r#struct;
 pub mod r#trait;
 
 pub use associated_types::*;
-pub use r#enum::*;
 pub use field::*;
 pub use function::*;
 pub use generics::*;
-pub use r#impl::*;
 pub use module::*;
+pub use r#enum::*;
+pub use r#impl::*;
 pub use r#struct::*;
 pub use r#trait::*;
